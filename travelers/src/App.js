5b91c0800703c27f6b87241b90a74132b0ce2201
Gonzalo Bonelli <gonzalourielbonelli@hotmail.com>
2022-10-04 18:32:07 -0300

    Implementacion de footer y sus estilos

--- travelers/src/App.js
+++ travelers/src/App.js
@@ -1,7 +1,11 @@
 import Destiny from "./Destiny";
 import Offer from "./Offer";
 import Exp from "./Exp";
-import "./styles/styles.css"
+import facebook from "./styles/facebook.png";
+import twitter from "./styles/twitter.png";
+import instagram from "./styles/instagram.png";
+import "./styles/styles.css";
+
 function App() {
   return (
     <div className="App">
@@ -38,6 +42,26 @@ function App() {
         </nav>
         <Exp imagenProfile="Joaquina" imagen="Hallstat-Austria"></Exp>
         <Exp imagenProfile="Rodolfa" imagen="Dolomites-Italy"></Exp>
+        <Exp imagenProfile="Rodolfa" imagen="Madrid"></Exp>
+        <footer>
+          <h2 className="footer-title">Travelers</h2>
+            <section className="section-1">
+              <a href="#">Destinos</a>
+              <a href="#">Hospedajes</a>
+              <a href="#">Experiencias</a>
+            </section>
+            <section className="section-2"> 
+              <a href="#">Conocenos</a>
+              <a href="#">Socios</a>
+              <a href="#">Terminos y Condiciones</a>
+            </section>
+            <section className="section-3">
+              <p>Contactanos</p>
+              <a href="#"><img className="img-contact" src={facebook}></img></a>
+              <a href="#"><img className="img-contact" src={twitter}></img></a>
+              <a href="#"><img className="img-contact" src={instagram}></img></a>
+            </section>
+        </footer>
     </div>
   );
 }
